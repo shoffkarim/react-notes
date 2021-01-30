3f52c39b763d082308aee5a5556b5d0696fc6686
shoffkarim <k4r1mch1k@gmail.com>
2021-01-30 13:38:38 +0500

    первый набросок верстки

--- src/App.js
+++ src/App.js
@@ -5,19 +5,33 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="logo">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <div className="app-name">
+          <p>React Notes</p>
+        </div>
+        <div className="notes-search">
+          <input type="text" placeholder="Поиск заметки"/>
+        </div>
       </header>
+      <main className="main">
+        <aside className="notes-aside">
+          <ul className="notes-list">
+            <li className="notes-item">
+              <p className="notes-item__name">Работа</p>
+              <span className="notes-item__date">30.01.2021</span>
+              <span className="notes-item__tag">#work</span>
+            </li>
+          </ul>
+        </aside>
+        <section className="workspace">
+          <div className="workspace-date">
+            <p>30 января 2021г., 12:13</p>
+          </div>
+          <div className="workspace-field"></div>
+        </section>
+      </main>
     </div>
   );
 }
